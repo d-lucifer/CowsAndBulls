33366f4f18a36488b101255bb7bc8e373ccd679b
ASUS <ASUS@ASUSZENBOOK>
2022-05-26 01:17:12 +0300

    Добавьте файлы проекта.

---
+++ Курсова/Form1.Designer.cs
@@ -0,0 +1,249 @@
+﻿
+namespace Курсова
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.файлToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.початиГруToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.withPCToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.зДругомToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.новаГраToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.labelpl1 = new System.Windows.Forms.Label();
+            this.labelpl2 = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.GripMargin = new System.Windows.Forms.Padding(2, 2, 0, 2);
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.файлToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(836, 33);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // файлToolStripMenuItem
+            // 
+            this.файлToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.початиГруToolStripMenuItem,
+            this.новаГраToolStripMenuItem});
+            this.файлToolStripMenuItem.Name = "файлToolStripMenuItem";
+            this.файлToolStripMenuItem.Size = new System.Drawing.Size(69, 29);
+            this.файлToolStripMenuItem.Text = "Файл";
+            // 
+            // початиГруToolStripMenuItem
+            // 
+            this.початиГруToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.withPCToolStripMenuItem,
+            this.зДругомToolStripMenuItem});
+            this.початиГруToolStripMenuItem.Name = "початиГруToolStripMenuItem";
+            this.початиГруToolStripMenuItem.Size = new System.Drawing.Size(270, 34);
+            this.початиГруToolStripMenuItem.Text = "Почати гру";
+            this.початиГруToolStripMenuItem.Click += new System.EventHandler(this.початиГруToolStripMenuItem_Click);
+            // 
+            // withPCToolStripMenuItem
+            // 
+            this.withPCToolStripMenuItem.Name = "withPCToolStripMenuItem";
+            this.withPCToolStripMenuItem.Size = new System.Drawing.Size(270, 34);
+            this.withPCToolStripMenuItem.Text = "З ПК";
+            this.withPCToolStripMenuItem.Click += new System.EventHandler(this.withPCToolStripMenuItem_Click);
+            // 
+            // зДругомToolStripMenuItem
+            // 
+            this.зДругомToolStripMenuItem.Name = "зДругомToolStripMenuItem";
+            this.зДругомToolStripMenuItem.Size = new System.Drawing.Size(270, 34);
+            this.зДругомToolStripMenuItem.Text = "З другом";
+            this.зДругомToolStripMenuItem.Click += new System.EventHandler(this.зДругомToolStripMenuItem_Click);
+            // 
+            // новаГраToolStripMenuItem
+            // 
+            this.новаГраToolStripMenuItem.Name = "новаГраToolStripMenuItem";
+            this.новаГраToolStripMenuItem.Size = new System.Drawing.Size(270, 34);
+            this.новаГраToolStripMenuItem.Text = "Нова гра";
+            this.новаГраToolStripMenuItem.Click += new System.EventHandler(this.новаГраToolStripMenuItem_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.textBox2.Location = new System.Drawing.Point(657, 331);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(135, 26);
+            this.textBox2.TabIndex = 2;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.textBox1.Location = new System.Drawing.Point(43, 331);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(135, 26);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Modern No. 20", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(213, 54);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(390, 43);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "ЗАГАДАНЕ ЧИСЛО";
+            // 
+            // labelpl1
+            // 
+            this.labelpl1.AutoSize = true;
+            this.labelpl1.BackColor = System.Drawing.Color.Yellow;
+            this.labelpl1.Location = new System.Drawing.Point(39, 299);
+            this.labelpl1.Name = "labelpl1";
+            this.labelpl1.Size = new System.Drawing.Size(85, 20);
+            this.labelpl1.TabIndex = 4;
+            this.labelpl1.Text = "Гравець 1";
+            // 
+            // labelpl2
+            // 
+            this.labelpl2.AutoSize = true;
+            this.labelpl2.BackColor = System.Drawing.Color.MediumSeaGreen;
+            this.labelpl2.Location = new System.Drawing.Point(653, 299);
+            this.labelpl2.Name = "labelpl2";
+            this.labelpl2.Size = new System.Drawing.Size(85, 20);
+            this.labelpl2.TabIndex = 6;
+            this.labelpl2.Text = "Гравець 2";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.checkBox1.Location = new System.Drawing.Point(43, 375);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(22, 21);
+            this.checkBox1.TabIndex = 7;
+            this.checkBox1.UseVisualStyleBackColor = false;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.checkBox2.Location = new System.Drawing.Point(657, 375);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(22, 21);
+            this.checkBox2.TabIndex = 8;
+            this.checkBox2.UseVisualStyleBackColor = false;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.richTextBox1.Location = new System.Drawing.Point(209, 258);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(100, 218);
+            this.richTextBox1.TabIndex = 9;
+            this.richTextBox1.Text = "";
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.richTextBox2.Location = new System.Drawing.Point(541, 258);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(100, 218);
+            this.richTextBox2.TabIndex = 4;
+            this.richTextBox2.Text = "";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(373, 426);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(110, 50);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "Перевірити";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(836, 494);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.labelpl2);
+            this.Controls.Add(this.labelpl1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "CowsAndBulls";
+            this.Resize += new System.EventHandler(this.зДругомToolStripMenuItem_Click);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem файлToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem початиГруToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem withPCToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem зДругомToolStripMenuItem;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label labelpl1;
+        private System.Windows.Forms.Label labelpl2;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ToolStripMenuItem новаГраToolStripMenuItem;
+        
+    }
+}
+
---
+++ Курсова/Form1.cs
@@ -0,0 +1,341 @@
+﻿using System;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace Курсова
+{
+    public partial class Form1 : Form
+    {
+        private int cows;
+        private int bull;
+        private string rn;
+        Random rand = new Random();
+        public Form1()
+        {
+            InitializeComponent();
+            label1.Font.Size.Equals(20);
+            label1.Text = "Загадане число";
+            label1.Visible = true;
+            checkBox1.Checked = false;
+            checkBox2.Checked = false;
+            textBox2.Enabled = false;
+            textBox1.Enabled = false;
+            button1.Enabled = false;
+
+        }
+
+        public void modePC(string userNum, string random)
+        {
+            rn = random;
+            textBox1.Enabled = true;
+            bool notWinner = true;
+            string pcNum = Convert.ToString(rand.Next(1000, 9999)); // число пк конвертоване в тип string
+            userNum = textBox1.Text;
+            textBox2.Text = pcNum;
+            checkBox2.Checked = true;
+            char[] rNums = rn.ToCharArray(); //угадуване число
+            char[] userGuess = userNum.ToCharArray(); //число гравця
+            char[] pcGuess = pcNum.ToCharArray(); // число пк
+            while (notWinner)
+            {
+                char[] result = new char[4];
+                for (int i = 0; i < pcNum.Length; i++)// PC
+                {
+                    if (pcGuess[i] == rNums[i])//якщо відгадана правильна позиція
+                    {
+                        bull += 1;
+                        result[i] = pcGuess[i];
+
+                    }
+                    else if (pcGuess.Contains(rNums[i]))
+                    {
+                        cows += 1;
+                        result[i] = '*';
+                    }
+                    else
+                    {
+                        result[i] = '*';
+                    }
+                    richTextBox2.Text += Convert.ToString(result[i]);
+                    if (i == 3)
+                    {
+                        richTextBox2.Text += "\n";
+                    }
+                }
+                if (richTextBox2.Text == label1.Text) //Перевірка чи відповідь пк рівна загадоному числу
+                {
+                    labelpl2.Text = "Переможець!";
+                    labelpl1.Text = "Програв!";
+                    button1.Enabled = false;
+                    notWinner = false;
+                    label1.Visible = true;
+
+                }
+                else if (richTextBox1.Text == label1.Text)
+                {
+                    labelpl1.Text = "Переможець!";
+                    labelpl2.Text = "Програв!";
+                    button1.Enabled = false;
+                    notWinner = false;
+                    label1.Visible = true;
+                }
+                checkBox2.Text = "Биків " + bull + " , Корів " + cows;
+                bull = 0;
+                cows = 0;
+
+                for (int i = 0; i < userNum.Length; i++)
+                {
+                    if (userGuess[i] == rNums[i])//якщо відгадана правильна позиція
+                    {
+                        bull += 1;
+                        result[i] = userGuess[i];
+
+                    }
+                    else if (rNums.Contains(userGuess[i]))
+                    {
+                        cows += 1;
+                        result[i] = '*';
+                    }
+                    else
+                    {
+                        result[i] = '*';
+                    }
+                    richTextBox1.Text += Convert.ToString(result[i]);
+                    if (i == 3)
+                    {
+                        richTextBox1.Text += "\n";
+                    }
+                }
+                if (textBox1.Text == rn) //Перевірка чи відповідь пк рівна загадоному числу
+                {
+                    labelpl1.Text = "Переможець!";
+                    labelpl2.Text = "Програв!";
+                    notWinner = false;
+                    button1.Enabled = false;
+                    label1.Visible = true;
+                }
+                else if (textBox2.Text == rn)
+                {
+                    labelpl1.Text = "Програв!";
+                    labelpl2.Text = "Переможець!";
+                    notWinner = false;
+                    button1.Enabled = false;
+                    label1.Visible = true;
+                }//for
+
+                checkBox1.Text = "Биків " + bull + " , Корів " + cows;
+                notWinner = false;
+                textBox1.Visible = true;
+                richTextBox1.Visible = true;
+            }//while 
+            bull = 0;
+            cows = 0;
+            // labelpl1.Text = "Хід першого гравця!";
+        }
+
+        public void withFriend(string numUser1, string numUser2, string random)
+        {
+            if (textBox1.Text.Length < 4)
+            {
+                MessageBox.Show("Потрібно ввести 4-значчне число!");
+            }
+            rn = random;
+            bool notWinner = true;
+            numUser1 = textBox1.Text;
+            numUser2 = textBox2.Text;
+
+            char[] rNums = rn.ToCharArray(); //угадуване число
+            char[] user1Guess = numUser1.ToCharArray(); //число гравця
+            char[] user2Guess = numUser2.ToCharArray(); // число пк
+            while (notWinner)
+            {
+                char[] result = new char[4];
+                for (int i = 0; i < numUser1.Length; i++)// PC
+                {
+
+                    if (user1Guess[i] == rNums[i])//якщо відгадана правильна позиція
+                    {
+                        bull += 1;
+                        result[i] = user1Guess[i];
+                    }
+                    else if (user1Guess.Contains(rNums[i]))
+                    {
+                        cows += 1;
+                        result[i] = '*';
+                    }
+                    else
+                    {
+                        result[i] = '*';
+                    }
+                    richTextBox1.Text += Convert.ToString(result[i]);
+                    if (i == 3)
+                    {
+                        richTextBox1.Text += "\n";
+                    }
+
+                }
+                if (richTextBox1.Text == label1.Text) //Перевірка чи відповідь пк рівна загадоному числу
+                {
+                    labelpl1.Text = "Переможець!";
+                    labelpl2.Text = "Програв!";
+                    button1.Enabled = false;
+                    notWinner = false;
+                    label1.Visible = true;
+
+                }
+                else if (richTextBox2.Text == label1.Text)
+                {
+                    labelpl2.Text = "Переможець!";
+                    labelpl1.Text = "Програв!";
+                    button1.Enabled = false;
+                    notWinner = false;
+                    label1.Visible = true;
+                }
+                checkBox1.Text = "Биків " + bull + " , Корів " + cows;
+                bull = 0;
+                cows = 0;
+
+                for (int i = 0; i < numUser2.Length; i++)
+                {
+                    if (user2Guess[i] == rNums[i])//якщо відгадана правильна позиція
+                    {
+                        bull += 1;
+                        result[i] = user2Guess[i];
+
+                    }
+                    else if (rNums.Contains(user2Guess[i]))
+                    {
+                        cows += 1;
+                        result[i] = '*';
+                    }
+                    else
+                    {
+                        result[i] = '*';
+                    }
+                    richTextBox2.Text += Convert.ToString(result[i]);
+                    if (i == 3)
+                    {
+                        richTextBox2.Text += "\n";
+                    }
+                }
+                if (textBox2.Text == rn) //Перевірка чи відповідь пк рівна загадоному числу
+                {
+                    labelpl2.Text = "Переможець!";
+                    labelpl1.Text = "Програв!";
+                    notWinner = false;
+                    button1.Enabled = false;
+                    label1.Visible = true;
+                }
+                else if (textBox1.Text == rn)
+                {
+                    labelpl2.Text = "Програв!";
+                    labelpl1.Text = "Переможець!";
+                    notWinner = false;
+                    button1.Enabled = false;
+                    label1.Visible = true;
+                }
+
+                checkBox2.Text = "Биків " + bull + " , Корів " + cows;
+                notWinner = false;
+                textBox1.Visible = true;
+                richTextBox1.Visible = true;
+            }
+            bull = 0;
+            cows = 0;
+            textBox1.Enabled = true;
+        }
+
+
+        private void зДругомToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            labelpl2.Text = "Гравець 2";
+
+            textBox1.Visible = true;
+
+
+        }
+        private void withPCToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            labelpl2.Text = "Комп'ютер";
+            textBox2.Enabled = false;
+            richTextBox2.Visible = false;
+            label1.Visible = false;
+            textBox1.Enabled = true;
+            textBox1.Visible = true;
+            richTextBox1.Visible = true;
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (labelpl2.Text == "Комп'ютер")
+            {
+                modePC(textBox1.Text, label1.Text);
+            }
+            else if (labelpl2.Text == "Гравець 2")
+            {
+                withFriend(textBox1.Text, textBox2.Text, label1.Text);
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            if (textBox1.Text.Length == 4)
+            {
+                checkBox1.Checked = true;
+                textBox1.Visible = false;
+                richTextBox1.Visible = false;
+                textBox2.Visible = true;
+                richTextBox2.Visible = true;
+                checkBox2.Checked = false;
+                labelpl1.Text = "Хід другого гравця!";
+                if (labelpl2.Text == "Комп'ютер")
+                {
+                    richTextBox2.Visible = false;
+                }
+            }
+        }
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            if (textBox2.Text.Length == 4)
+            {
+                checkBox2.Checked = true;
+                checkBox1.Checked = true;
+                labelpl1.Text = "Хід першого гравця!";
+            }
+            if (checkBox2.Checked == true)
+            {
+                textBox2.Visible = false;
+                richTextBox2.Visible = false;
+                textBox1.Visible = false;
+                richTextBox1.Visible = false;
+            }
+        }
+        private void новаГраToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            label1.Visible = false;
+            richTextBox1.Text = " ";
+            richTextBox2.Text = " ";
+            textBox1.Text = " ";
+            textBox2.Text = " ";
+            labelpl1.Text = "Гравець 1";
+            labelpl2.Text = "Гравець 2";
+            checkBox1.Checked = false;
+            checkBox1.Text = " ";
+            checkBox2.Text = " ";
+            checkBox2.Checked = false;
+        }
+
+        private void початиГруToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            label1.Text = Convert.ToString(rand.Next(1000, 9999));
+            labelpl1.Text = "Хід першого гравця!";
+            label1.Visible = false;
+            textBox2.Enabled = true;
+            textBox1.Enabled = true;
+            textBox1.Visible = true;
+            textBox2.Visible = true;
+            richTextBox1.Visible = true;
+            richTextBox2.Visible = true;
+            button1.Enabled = true;
+        }
+    }
+}
